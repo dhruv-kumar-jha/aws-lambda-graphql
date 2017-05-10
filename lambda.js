7d51ed1df62da5c81427d792aaf3a0f1b9b07707
Dhruv Kumar Jha <dhruv@dhruvkumarjha.com>
2017-05-10 13:18:27 +0530

    Updated lambda.js file

--- lambda.js
+++ lambda.js
@@ -1,11 +1,7 @@
 'use strict'
 
-console.log('starting up');
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./src/app');
+const server = awsServerlessExpress.createServer(app);
 
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./src/app')
-const server = awsServerlessExpress.createServer(app)
-
-console.log('after server line');
-
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
